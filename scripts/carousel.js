795fad93e86607c26f7e2aed6da13ec7783a149c
krunutty <noppanut.joi@cru.ac.th>
2025-03-14 08:39:58 +0700

    Add files via upload

---
+++ scripts/carousel.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    spaceBetween: 30,
+
+    autoplay: {
+        delay: 5000
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        },
+    }
+  });
